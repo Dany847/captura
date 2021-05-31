e1459c0c581e1882b1134a1918feb9fe2007a787
Dany847 <danieldelacruz-07@hotmail.com>
2021-05-31 00:51:01 -0500

    ordenar codigos

--- update/edit_zona.php
+++ update/edit_zona.php
@@ -1,17 +1,24 @@
 	<?php 
 	require_once '../connect/conexion.php';
 	if (ISSET($_POST['editar'])) {
-		$nombre = $_POST['nombre'];
-		$responsable = $_POST['responsable'];
-		$query =  $conn->query("SELECT * FROM `zonas` WHERE `nombre` = '$nombre' && `responsable` = '$responsable'") or die(mysql_error());
+
+		$id_nivel = $_POST['id_nivel'];
+		$numero_zona = $_POST['numero_zona'];
+		$nombre_zona = $_POST['nombre_zona'];
+		$nombre_responsable = $_POST['nombre_responsable'];
+		$apellido_paterno = $_POST['apellido_paterno'];
+		$apellido_materno = $_POST['apellido_materno'];
+
+
+		$query =  $conn->query("SELECT * FROM `zona` WHERE `numero_zona` = '$numero_zona' && `nombre_zona` = '$nombre_zona'") or die(mysql_error());
 		$q = $query->num_rows;
 		if ($q == 1) {
 			echo ' <script type="text/javascript">
-			alert("Nombre del zona ya existe");
+			alert("Nombre de zona ya existe");
 			window.location = "../vistas/zona.php";
 			</script>';
 		}else{
-			$conn->query("UPDATE `zonas` SET `nombre` = '$nombre', `responsable` = '$responsable' WHERE `id_zona` = '$_REQUEST[id_zona]' ") or die(mysql_error());
+			$conn->query("UPDATE `zona` SET `id_nivel` = '$id_nivel', `numero_zona` = '$numero_zona', `nombre_zona` = '$nombre_zona', `nombre_responsable` = '$nombre_responsable', `apellido_paterno` = '$apellido_paterno', `apellido_materno` = '$apellido_materno' WHERE `id_zona` = '$_REQUEST[id_zona]' ") or die(mysql_error());
 			echo ' <script type="text/javascript">
 			alert("Datos Actualizado Exitosamente");
 			window.location = "../vistas/zona.php";
